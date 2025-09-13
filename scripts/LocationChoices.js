c6e5594dbcfc2081f497b70c331f849af2ab8787
James Freeman <jamesfreeman114@gmail.com>
2025-09-13 13:39:52 -0500

    Added Location Choices Component

---
+++ scripts/LocationChoices.js
@@ -0,0 +1,22 @@
+export const LocationChoices = async () => {
+    const response = await fetch("http://localhost:8088/socioLocations")
+    const locations = await response.json()
+
+
+    let html = `
+        <div class ="survey-input">
+            <h2>What type of area do you live in?</h2>
+
+    `
+
+    for (const location of locations) {
+        html += `<input type ="radio" name ="location" value="${location.id}"/>${location.label} `
+        
+    }
+    
+    html += `
+        </div>
+    `
+    return html 
+}
+
